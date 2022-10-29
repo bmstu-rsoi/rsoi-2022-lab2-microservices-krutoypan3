8c36f15396007334b9ad78f02b5ccdac36213cb7
Artem Oganesyan <Krutoypan3@gmail.com>
2022-10-30 01:00:50 +0300

    Раскинул сервера спринга по разным контейнерам

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM adoptopenjdk/openjdk11
-WORKDIR /app
-COPY ./ app/
-EXPOSE 8080
-ENTRYPOINT ["sh", "app/invoke.sh"]
--- invoke.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-java -Xmx128m -jar -Dserver.port=8070 app/spring-boot-microservice-reservation-system/build/libs/spring-boot-microservice-reservation-system.jar &
-java -Xmx128m -jar -Dserver.port=8060 app/spring-boot-microservice-library-system/build/libs/spring-boot-microservice-library-system.jar &
-java -Xmx128m -jar -Dserver.port=8050 app/spring-boot-microservice-rating-system/build/libs/spring-boot-microservice-rating-system.jar &
-java -Xmx128m -jar -Dserver.port=8080 app/spring-boot-microservice-gateway/build/libs/spring-boot-microservice-gateway.jar
